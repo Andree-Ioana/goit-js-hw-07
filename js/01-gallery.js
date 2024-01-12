947e0ab9144ffb421d230218a549532f7d4a8cbf
Andree-Ioana <ioana.vinereanu@stud.etti.upb.ro>
2024-01-12 14:33:36 +0200

    finish ex1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+//Functia pentru crearea si randarea galeriei
+
+const gallery = document.querySelector("ul.gallery");
+
+function createGallery(){
+
+    galleryItems.forEach((item) => {
+        const liItem = document.createElement("li");
+        liItem.classList.add("gallery__item");
+
+        const link = document.createElement("a");
+        link.classList.add("gallery__link");
+        link.href = item.original;
+
+        const image = document.createElement("img");
+        image.classList.add("gallery__image");
+        image.src = item.preview;
+        image.alt = item.description;
+        image.setAttribute("data-source", item.original);
+
+        //Adaugam elementele create in DOM
+
+        link.appendChild(image);
+        liItem.appendChild(link);
+        gallery.appendChild(liItem);
+    });
+}
+
+createGallery();
+
+import basicLightbox from "https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/+esm";
+const instance = basicLightbox.create(` <img> `),
+               modalImage = instance.element().querySelector("img");
+               
+    gallery.addEventListener("click", (ev) => {
+      ev.preventDefault();
+      modalImage.src = ev.target.dataset.source;
+      instance.show();
+    });
